9d746f6d5fb4a56e2aaf4eae9444af55b70f19bc
Francesc Abbate <francesco.bbt@gmail.com>
2014-02-09 17:49:48 +0100

    Factor the reader code in a separate module

--- compile.lua
+++ compile.lua
@@ -2,35 +2,7 @@ local lex_setup = require('lexer')
 local parse = require('parser')
 local ast = require('lua-ast').New()
 local generator = require('generator')
-
-local strsub = string.sub
-
-local function new_string_reader()
-    local src, pos
-    local function init(src_in)
-        src, pos = src_in, 1
-    end
-    local function reader()
-        local chunk = strsub(src, pos, pos + 4096 - 32)
-        pos = pos + #chunk
-        return #chunk > 0 and chunk or nil
-    end
-    return init, reader
-end
-
-local function new_file_reader()
-    local f
-    local function init(filename)
-        f = assert(io.open(filename, 'r'), "cannot open file " .. filename)
-    end
-    local function reader()
-        return f:read(4096 - 32)
-    end
-    return init, reader
-end
-
-local string_reader_init, string_reader = new_string_reader()
-local file_reader_init, file_reader = new_file_reader()
+local reader = require('reader')
 
 local function compile(reader, filename)
     local ls = lex_setup(reader, filename)
@@ -46,25 +18,13 @@ local function compile(reader, filename)
 end
 
 local function lang_loadstring(src, filename)
-    string_reader_init(src)
-    return compile(string_reader, filename)
+    reader.string_init(src)
+    return compile(reader.string, filename)
 end
 
 local function lang_loadfile(filename)
-    file_reader_init(filename)
-    return compile(file_reader, filename)
-end
-
--- Used for the "use" statement to query the symbols defined in a given
--- module.
-function ast.probe(name)
-    local defs = { }
-    local mod = _G[name]
-    for k, v in pairs(mod) do
-        defs[k] = true
-    end
-    return defs
+    reader.file_init(filename)
+    return compile(reader.file, filename)
 end
 
 return { string = lang_loadstring, file = lang_loadfile }
-
