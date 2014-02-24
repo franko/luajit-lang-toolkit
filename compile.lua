fead3e909b25031d7f41a92e68496cb547320e68
Francesco Abbate <francesco.bbt@gmail.com>
2014-02-24 15:59:22 +0100

    Add command line options for to run the bytecode compiler.
    The option "-b" is implemented to optionally print bytecode dump.

--- compile.lua
+++ compile.lua
@@ -4,27 +4,29 @@ local ast = require('lua-ast').New()
 local generator = require('generator')
 local reader = require('reader')
 
-local function compile(reader, filename)
+local function compile(reader, filename, options)
     local ls = lex_setup(reader, filename)
     local tree = parse(ast, ls)
     local luacode = generator(tree, filename)
 
-    -- dump the bytecode
-    local jbc = require("jit.bc")
-    local fn = assert(loadstring(luacode))
-    jbc.dump(fn, nil, true)
+    if options.bcprint then
+        -- dump the bytecode
+        local jbc = require("jit.bc")
+        local fn = assert(loadstring(luacode))
+        jbc.dump(fn, nil, true)
+    end
 
     return luacode
 end
 
-local function lang_loadstring(src, filename)
+local function lang_loadstring(src, filename, options)
     reader.string_init(src)
-    return compile(reader.string, filename)
+    return compile(reader.string, filename, options)
 end
 
-local function lang_loadfile(filename)
+local function lang_loadfile(filename, options)
     reader.file_init(filename)
-    return compile(reader.file, filename)
+    return compile(reader.file, filename, options)
 end
 
 return { string = lang_loadstring, file = lang_loadfile }
--- run.lua
+++ run.lua
@@ -1,9 +1,38 @@
 local compile = require("compile")
-local filename = assert(..., "usage: luajit run.lua <filename>")
+
+local function print_usage_msg()
+    print [[
+usage: luajit run.lua <filename>
+
+Options:
+    -b      Dump bytecode informations.
+]]
+end
+
+local filename
+
+local args = {...}
+local options = { bcprint = false }
+local k = 1
+while args[k] do
+    if args[k] == "-b" then
+        table.remove(args, k)
+        options.bcprint = true
+    else
+        if string.sub(args[k], 1, 1) == "-" then
+            print("Invalid option: ", args[k])
+            print_usage_msg()
+        end
+        filename = args[k]
+        k = k + 1
+        if args[k] then print_usage_msg() end
+    end
+end
+
+if not filename then print_usage_msg() end
 
 -- Compute the bytecode string for the given filename.
-local luacode = compile.file(filename)
+local luacode = compile.file(filename, options)
 
 local fn = assert(loadstring(luacode))
 fn()
-
