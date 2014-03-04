a195e6288560525869c98ded08eeca9770410020
Francesco Abbate <francesco.bbt@gmail.com>
2014-03-04 13:19:17 +0100

    Improve one of the logical values test

--- tests/logical-value-1.lua
+++ tests/logical-value-1.lua
@@ -18,8 +18,11 @@ local function foo4(x, y)
     return x < y or x == y
 end
 
+local function diff(a, b) return a - b end
+
 local x, y = 3, 7
 print(foo1(x, y), foo1(y, x))
+print(foo1bis(x, y, diff), foo1bis(y, x, diff))
 print(foo2(x, y), foo2(y, x))
 print(foo3(x, y), foo3(y, x))
 print(foo4(x, y), foo4(y, x))
