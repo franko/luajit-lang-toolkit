d9a12fc43870624c9d577916a297195e4a2b3819
Francesco Abbate <francesco.bbt@gmail.com>
2014-03-04 13:17:09 +0100

    Modify output testing script to print the test results directly

--- scripts/bcgen-test.py
+++ scripts/bcgen-test.py
@@ -5,8 +5,6 @@ import subprocess
 test_dir = "tests"
 luajit_exec = "luajit"
 
-log = open("bcgen-log.txt", "w")
-
 for dirpath, dirnames, filenames in os.walk(test_dir):
     for filename in filenames:
         m = re.match(r'([^.]+)\.lua$', filename)
@@ -20,18 +18,19 @@ for dirpath, dirnames, filenames in os.walk(test_dir):
             except subprocess.CalledProcessError:
                 run_error = True
             out_ref = subprocess.check_output([luajit_exec, fullname])
-            msg = None
+            led, msg = None, None
             if run_error:
-                msg = "* %-24s\tFail to run.\n" % test_name
+                led, msg = "*", "fail to run"
             elif out_tst == out_ref:
                 if out_tst in ["", "\n", "\r\n"] or not out_tst:
-                    msg = "- %-24s\tPass / no output expected.\n" % test_name
+                    led, msg = "-", "pass / no output"
                 else:
-                    msg = "- %-24s\tPass.\n" % test_name
+                    led, msg = " ", "pass"
             else:
-                msg = "* %-24s\tDiffer:\n" % test_name
-                msg = msg + ("*** Reference ***\n%s\n" % out_ref)
-                msg = msg + ("*** Test Program ***\n%s\n" % out_tst)
-            log.write(msg)
+                led, msg = "*", "fail"
+                log = open("tests/log/%s.output.diff" % test_name, "w")
+                log.write("*** reference ***\n%s\n" % out_ref)
+                log.write("*** test program ***\n%s\n" % out_tst)
+                log.close()
 
-log.close()
+            print("%s %-24s %s" % (led, test_name, msg))
