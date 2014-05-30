bd0023d6d9da519a87459a0991a33b08a29cdaa7
Francesco Abbate <francesco.bbt@gmail.com>
2014-05-30 15:35:26 +0200

    Add a test to check expressions versus operator's prority

---
+++ tests/expr-prio-1.lua
@@ -0,0 +1,14 @@
+local x, y = 2, 3
+
+local a = (2 + x) * y
+local b = x + 2 * y
+local c = 2 ^ x ^ y
+local d = (2 ^ x) ^ y
+local e = 2 * x / y
+local f = 2 * (x / y)
+local g = 2 + x + y
+local h = 2 + (x + y)
+local i = -x * y
+local l = -x^2 + (-x)^2
+
+print(a, b, c, d, e, f, g, h, i, l)
