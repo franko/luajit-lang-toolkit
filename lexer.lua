4c0370788363a46a35afbddecffdba516fa7042f
Francesco Abbate <francesco.bbt@gmail.com>
2014-08-24 17:22:32 +0200

    Fix lexer for 64bit hex numbers
    Github issue:
    
    https://github.com/franko/luajit-lang-toolkit/issues/4

--- lexer.lua
+++ lexer.lua
@@ -4,7 +4,7 @@ local band = bit.band
 local strsub, strbyte, strchar = string.sub, string.byte, string.char
 
 local ASCII_0, ASCII_9 = 48, 57
-local ASCII_a, ASCII_z = 97, 122
+local ASCII_a, ASCII_f, ASCII_z = 97, 102, 122
 local ASCII_A, ASCII_Z = 65, 90
 
 local END_OF_STREAM = -1
@@ -172,8 +172,32 @@ local function build_64int(str)
     local u = str[#str - 2]
     local x = (u == 117 and uint64(0) or int64(0))
     local i = 1
-    while str[i] <= ASCII_9 do
+    while str[i] >= ASCII_0 and str[i] <= ASCII_9 do
         x = 10 * x + (str[i] - ASCII_0)
+        i = i + 1
+    end
+    return x
+end
+
+-- Only lower case letters are accepted.
+local function byte_to_hexdigit(b)
+    if b >= ASCII_0 and b <= ASCII_9 then
+        return b - ASCII_0
+    elseif b >= ASCII_a and b <= ASCII_f then
+        return 10 + (b - ASCII_a)
+    else
+        return -1
+    end
+end
+
+local function build_64hex(str)
+    local u = str[#str - 2]
+    local x = (u == 117 and uint64(0) or int64(0))
+    local i = 3
+    while str[i] do
+        local n = byte_to_hexdigit(str[i])
+        if n < 0 then break end
+        x = 16 * x + n
         i = i + 1
     end
     return x
@@ -214,7 +238,9 @@ local function lex_number(ls)
         if img then x = complex(0, img) end
     elseif strsub(str, -2, -1) == 'll' then
         local t = strnumdump(str)
-        if t then x = build_64int(t) end
+        if t then
+            x = xp == 'e' and build_64int(t) or build_64hex(t)
+        end
     else
         x = tonumber(str)
     end
