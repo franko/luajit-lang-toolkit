22cc38727651cf13fdb09f32dc48c9781a6f30f1
Francesco Abbate <francesco.bbt@gmail.com>
2014-11-18 13:57:51 +0100

    Fix file open flags for bcread-file.lua on Windows
    The "b" flag is required on windows because it is a binary file.

--- bcread-file.lua
+++ bcread-file.lua
@@ -1,7 +1,7 @@
 local bcread = require("lang.bcread")
 
 local filename = ...
-local f = io.open(filename, "r")
+local f = io.open(filename, "rb")
 local s = f:read("*a")
 bcread.start(s)
 f:close()
