franko/luajit-lang-toolkit

commit 2e5566036b835f2cc0b94321b0120439209555ed

Remove spurious tonumber conversion from bytecode implementation

bytecode.lua
@@ -263,7 +263,6 @@ Buf.__index.put_uint32 = function(self, v)
 end
 
 Buf.__index.put_uleb128 = function(self,  v)
-    v = tonumber(v)
     local offs = self.offs
     local b = bit.band(v, 0x7f)
     v = bit.rshift(v, 7)
@@ -281,7 +280,6 @@ end
 -- Write a 32 bit unsigned integer + 1 bit given by "numbit".
 -- This last argument should be either 0 or 1.
 Buf.__index.put_uleb128_33 = function(self,  v, numbit)
-    v = tonumber(v)
     local offs = self.offs
     local b = bit.bor(bit.lshift(bit.band(v, 0x3f), 1), numbit)
     v = bit.rshift(v, 6)

commit 52e26367b551f06de2c562276b4b11c17f6110ab

Fix problem introduced by 2e556603 on 64bit systems
Use "unsigned ints" instead of size_t to avoid 64 bit boxed integers.

bytecode.lua
@@ -193,8 +193,8 @@ ffi.cdef[[
     int free(void*);
 
     typedef struct Buf {
-        size_t size;
-        size_t offs;
+        unsigned int size;
+        unsigned int offs;
         uint8_t *data;
     } Buf;
 ]]