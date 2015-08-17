franko/luajit-lang-toolkit

commit fa33b63abb3bb54ced3801494843bfb62110093b

Enable Lua code generation from command line

lang/compile.lua
@@ -11,7 +11,7 @@ local reader = require('lang.reader')
 -- a string that can be passed to the function "loadstring".
 -- In the case of the bytecode generator the string will be actually a
 -- binary blob that corresponds to the generated bytecode.
-local generator = require('lang.generator')
+
 
 local function lang_toolkit_error(msg)
    if string.sub(msg, 1, 9) == "LLT-ERROR" then
@@ -22,6 +22,12 @@ local function lang_toolkit_error(msg)
 end
 
 local function compile(reader, filename, options)
+    local generator
+    if options.code then
+        generator = require('lang.luacode-generator')
+    else
+        generator = require('lang.generator')
+    end
     local ls = lex_setup(reader, filename)
     local parse_success, tree = pcall(parse, ast, ls)
     if not parse_success then

run.lua
@@ -4,6 +4,7 @@ LuaJIT Language Toolkit usage: luajit [options]... [script [args]...].
 
 Available options are:
   -b ...    Save or list bytecode.
+  -c ...    Generate Lua code and run.
 ]]
   os.exit(1)
 end
@@ -18,28 +19,31 @@ local function check(success, result)
 end
 
 local args = {...}
+local opt = {}
 local k = 1
 while args[k] do
     local a = args[k]
-    if type(a) == "string" and string.sub(a, 1, 2) == "-b" then
-        local j = 1
-        if #a > 2 then
-            args[j] = "-" .. string.sub(a, 3)
-            j = j + 1
+    if string.sub(args[k], 1, 1) == "-" then
+        if string.sub(a, 2, 2) == "b" then
+            local j = 1
+            if #a > 2 then
+                args[j] = "-" .. string.sub(a, 3)
+                j = j + 1
+            else
+                table.remove(args, j)
+            end
+            require("lang.bcsave").start(unpack(args))
+            os.exit(0)
+        elseif string.sub(a, 2, 2) == "c" then
+            opt = { code = true }
         else
-            table.remove(args, j)
-        end
-        require("lang.bcsave").start(unpack(args))
-        os.exit(0)
-    else
-        if string.sub(args[k], 1, 1) == "-" then
             print("Invalid option: ", args[k])
-            print_usage_msg()
+            usage()
         end
+    else
         filename = args[k]
-        k = k + 1
-        if args[k] then usage() end
     end
+    k = k + 1
 end
 
 if not filename then usage() end
@@ -47,6 +51,11 @@ if not filename then usage() end
 local compile = require("lang.compile")
 
 -- Compute the bytecode string for the given filename.
-local luacode = check(compile.file(filename))
+local luacode = check(compile.file(filename, opt))
+if opt.code then
+    print(luacode)
+    print('\n\nOutput:')
+end
 local fn = assert(loadstring(luacode))
 fn()
+

commit 8f1b209db2de5cfd5c14c759e084d2d405ae48b9

Fix problem with handling options in "compile" module
Introduced with commit fa33b63abb.

lang/compile.lua
@@ -23,7 +23,7 @@ end
 
 local function compile(reader, filename, options)
     local generator
-    if options.code then
+    if options and options.code then
         generator = require('lang.luacode-generator')
     else
         generator = require('lang.generator')