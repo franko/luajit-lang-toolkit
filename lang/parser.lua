franko/luajit-lang-toolkit

commit 766aba8121a8143e5b55f82b85d5b3b8c35d91ac

Implement functions with keywords arguments

lang/parser.lua
@@ -115,7 +115,7 @@ function expr_simple(ast, ls)
         return expr_table(ast, ls)
     elseif tk == 'TK_function' then
         ls:next()
-        local args, body, proto = parse_body(ast, ls, ls.linenumber, false)
+        local args, body, proto = parse_body(ast, ls, ls.linenumber, false, true)
         return ast:expr_function(args, body, proto)
     elseif tk == '|' then
         local args, body, proto = parse_simple_body(ast, ls, ls.linenumber)
@@ -194,11 +194,11 @@ function expr_primary(ast, ls)
         elseif ls.token == ':' then
             ls:next()
             local key = lex_str(ls)
-            local args = parse_args(ast, ls)
+            local args = parse_args(ast, ls, false)
             vk, v = 'call', ast:expr_method_call(v, key, args, line)
         elseif ls.token == '(' or ls.token == 'TK_string' or ls.token == '{' then
-            local args = parse_args(ast, ls)
-            vk, v = 'call', ast:expr_function_call(v, args, line)
+            local args, kwargs = parse_args(ast, ls, true)
+            vk, v = 'call', ast:expr_function_call(v, args, kwargs, line)
         else
             break
         end
@@ -281,19 +281,36 @@ local function parse_repeat(ast, ls, line)
     return ast:repeat_stmt(cond, body, line, lastline)
 end
 
+local function expr_keyword(ast, ls, accept_keywords)
+    local kw, val
+    if ls.token == "TK_name" and accept_keywords and ls:lookahead() == '=' then
+        local name = lex_str(ls)
+        kw = ast:literal(name)
+        lex_check(ls, '=')
+    end
+    val = expr(ast, ls)
+    return val, kw
+end
+
 -- Parse function argument list.
-function parse_args(ast, ls)
+function parse_args(ast, ls, accept_keywords)
     local line = ls.linenumber
-    local args
+    local args = { }
+    local kwargs
     if ls.token == '(' then
         if not LJ_52 and line ~= ls.lastline then
             err_syntax(ls, "ambiguous syntax (function call x new statement)")
         end
         ls:next()
-        if ls.token ~= ')' then -- Not f().
-            args = expr_list(ast, ls)
-        else
-            args = { }
+        while ls.token ~= ')' do
+            local val, kw = expr_keyword(ast, ls, accept_keywords)
+            if kw then
+                if not kwargs then kwargs = {} end
+                kwargs[#kwargs+1] = { val, kw }
+            else
+                args[#args+1] = val
+            end
+            if not lex_opt(ls, ',') then break end
         end
         lex_match(ls, ')', '(', line)
     elseif ls.token == '{' then
@@ -306,7 +323,7 @@ function parse_args(ast, ls)
     else
         err_syntax(ls, "function arguments expected")
     end
-    return args
+    return args, kwargs
 end
 
 local function parse_assignment(ast, ls, vlist, var, vk)
@@ -338,7 +355,7 @@ local function parse_local(ast, ls)
     local line = ls.linenumber
     if lex_opt(ls, 'TK_function') then -- Local function declaration.
         local name = lex_str(ls)
-        local args, body, proto = parse_body(ast, ls, line, false)
+        local args, body, proto = parse_body(ast, ls, line, false, true)
         return ast:local_function_decl(name, args, body, proto)
     else -- Local variable declaration.
         local vl = { }
@@ -367,7 +384,7 @@ local function parse_func(ast, ls, line)
         needself = true
         v = expr_field(ast, ls, v)
     end
-    local args, body, proto = parse_body(ast, ls, line, needself)
+    local args, body, proto = parse_body(ast, ls, line, needself, not needself)
     return ast:function_decl(v, args, body, proto)
 end
 
@@ -479,9 +496,10 @@ local function parse_stmt(ast, ls)
     return stmt, false
 end
 
-local function parse_params_delim(ast, ls, needself, start_token, end_token)
+local function parse_params_delim(ast, ls, needself, accept_keywords, start_token, end_token)
     lex_check(ls, start_token)
     local args = { }
+    local kwargs
     if needself then
         args[1] = ast:var_declare("self")
     end
@@ -489,7 +507,14 @@ local function parse_params_delim(ast, ls, needself, start_token, end_token)
         repeat
             if ls.token == 'TK_name' or (not LJ_52 and ls.token == 'TK_goto') then
                 local name = lex_str(ls)
-                args[#args+1] = ast:var_declare(name)
+                if accept_keywords and lex_opt(ls, '=') then
+                    local kwarg_default = expr(ast, ls)
+                    if not kwargs then kwargs = {} end
+                    ast:var_declare(name)
+                    kwargs[#kwargs+1] = { kwarg_default, ast:literal(name) }
+                else
+                    args[#args+1] = ast:var_declare(name)
+                end
             elseif ls.token == 'TK_dots' then
                 ls:next()
                 ls.fs.varargs = true
@@ -501,11 +526,12 @@ local function parse_params_delim(ast, ls, needself, start_token, end_token)
         until not lex_opt(ls, ',')
     end
     lex_check(ls, end_token)
+    args.kwargs = kwargs
     return args
 end
 
-local function parse_params(ast, ls, needself)
-    return parse_params_delim(ast, ls, needself, '(', ')')
+local function parse_params(ast, ls, needself, accept_keywords)
+    return parse_params_delim(ast, ls, needself, accept_keywords, '(', ')')
 end
 
 local function new_proto(ls, varargs)
@@ -530,12 +556,12 @@ local function parse_chunk(ast, ls)
 end
 
 -- Parse body of a function.
-function parse_body(ast, ls, line, needself)
+function parse_body(ast, ls, line, needself, accept_keywords)
     local pfs = ls.fs
     ls.fs = new_proto(ls, false)
     ast:fscope_begin()
     ls.fs.firstline = line
-    local args = parse_params(ast, ls, needself)
+    local args = parse_params(ast, ls, needself, accept_keywords)
     local body = parse_block(ast, ls)
     ast:fscope_end()
     local proto = ls.fs
@@ -553,7 +579,7 @@ function parse_simple_body(ast, ls, line)
     ls.fs = new_proto(ls, false)
     ast:fscope_begin()
     ls.fs.firstline = line
-    local args = parse_params_delim(ast, ls, false, '|', '|')
+    local args = parse_params_delim(ast, ls, false, true, '|', '|')
     ls.fs.lastline = ls.linenumber
     local exp = expr(ast, ls)
     local ret_stmt = ast:return_stmt({ exp }, line)

lang/lua-ast.lua
@@ -51,17 +51,84 @@ local function func_expr(body, params, vararg, firstline, lastline)
     return build("FunctionExpression", { body = body, params = params, vararg = vararg, firstline = firstline, lastline = lastline })
 end
 
+local function list_extend(ls, src)
+    local n = #ls
+    for i = 1, #src do ls[n+i] = src[i] end
+    return ls
+end
+
+local function lookup_key_pair_node_list(kwargs, property)
+    for i = 1, #kwargs do
+        local kwname = kwargs[i][2].value
+        if kwname == property then
+            return kwargs[i][1]
+        end
+    end
+end
+
+local function build_option_from_node(table_node, property, default_expr_node_list, line)
+    local index_table = field(table_node, property, line)
+    local expr_first_clause = logical_binop("and", binop("~=", index_table, literal(nil), line), index_table, line)
+    return logical_binop("or", expr_first_clause, lookup_key_pair_node_list(default_expr_node_list, property), line)
+end
+
+local function keywords_func_helper_stmts(ast, id, body, args, kwargs, vararg, firstline, lastline)
+    local kwargs_id, nakedf_id, object_id, fallbf_id = ast:genid(), ast:genid(), ast:genid(), ast:genid()
+
+    local naked_func_args = list_extend({ kwargs_id }, args)
+
+    local kwids, kwvalues = {}, {}
+    for i = 1, #kwargs do
+        local kwname = kwargs[i][2].value
+        kwids[i] = ident(kwname)
+        kwvalues[i] = build_option_from_node(kwargs_id, kwname, kwargs, firstline)
+    end
+    local kw_vars_local_decl = build("LocalDeclaration", { names = kwids, expressions = kwvalues, line = firstline })
+
+    local naked_func_body = list_extend({ kw_vars_local_decl}, body)
+    local naked_func = func_decl(nakedf_id, naked_func_body, naked_func_args, vararg, true, firstline, lastline)
+
+    local fallback_args = list_extend({ object_id }, args)
+    local fallback_call_params = list_extend({ empty_table(firstline) }, args)
+    local fallback_call = build("CallExpression", { callee = nakedf_id, arguments = fallback_call_params, line = firstline })
+    local fallback_func_body = { build("ReturnStatement", { arguments = { fallback_call }, line = firstline }) }
+    local fallback_func = func_decl(fallbf_id, fallback_func_body, fallback_args, false, true, firstline, lastline)
+
+    local obj_table = build("Table", { keyvals = { { nakedf_id, literal("__kwcall") } }, line = firstline })
+    local obj_meta = build("Table", { keyvals = { { fallbf_id, literal("__call") } }, line = firstline })
+
+    local create_obj_call = build("CallExpression", { callee = ident("setmetatable"), arguments = { obj_table, obj_meta }, line = firstline })
+
+    local obj_assign = build("AssignmentExpression", { left = { id }, right = { create_obj_call }, line = firstline })
+    return { naked_func, fallback_func, obj_assign }
+end
+
+local function func_decl_keywords(ast, id, body, args, kwargs, vararg, locald, firstline, lastline)
+    local func_decl = build("LocalDeclaration", { names = { id } , expressions = { }, line = firstline })
+    local pre_stmts = keywords_func_helper_stmts(ast, id, body, args, kwargs, vararg, firstline, lastline)
+    local gen_stmts = locald and list_extend({ func_decl }, pre_stmts) or pre_stmts
+    return build("StatementsGroup", { statements = gen_stmts, line = firstline })
+end
+
 function AST.expr_function(ast, args, body, proto)
-   return func_expr(body, args, proto.varargs, proto.firstline, proto.lastline)
+    return func_expr(body, args, proto.varargs, proto.firstline, proto.lastline)
 end
 
 function AST.local_function_decl(ast, name, args, body, proto)
     local id = ast:var_declare(name)
-    return func_decl(id, body, args, proto.varargs, true, proto.firstline, proto.lastline)
+    if args.kwargs then
+        return func_decl_keywords(ast, id, body, args, args.kwargs, proto.varargs, true, proto.firstline, proto.lastline)
+    else
+        return func_decl(id, body, args, proto.varargs, true, proto.firstline, proto.lastline)
+    end
 end
 
 function AST.function_decl(ast, path, args, body, proto)
-   return func_decl(path, body, args, proto.varargs, false, proto.firstline, proto.lastline)
+    if args.kwargs then
+        return func_decl_keywords(ast, path, body, args, args.kwargs, proto.varargs, false, proto.firstline, proto.lastline)
+    else
+        return func_decl(path, body, args, proto.varargs, false, proto.firstline, proto.lastline)
+    end
 end
 
 function AST.chunk(ast, body, chunkname, firstline, lastline)
@@ -153,8 +220,18 @@ function AST.expr_method_call(ast, v, key, args, line)
     return build("SendExpression", { receiver = v, method = m, arguments = args, line = line })
 end
 
-function AST.expr_function_call(ast, v, args, line)
-    return build("CallExpression", { callee = v, arguments = args, line = line })
+function AST.expr_function_call(ast, v, args, kwargs, line)
+    if kwargs then
+        local kwt = build("Table", { keyvals = kwargs, line = line })
+        local ext_args = { kwt }
+        for i = 1, #args do
+            ext_args[i+1] = args[i]
+        end
+        local kw_callee = field(v, "__kwcall", line)
+        return build("CallExpression", { callee = kw_callee, arguments = ext_args, line = line })
+    else
+        return build("CallExpression", { callee = v, arguments = args, line = line })
+    end
 end
 
 function AST.return_stmt(ast, exps, line)

commit c4c271ea18e14597ae11bbb78e21f47f18bf72df

Fix error with bracketed expressions
The introduced error prevented correct generation of single-valued
expressions for bracketed expressions.

Error introduced with commit 766aba81.

lang/parser.lua
@@ -127,17 +127,52 @@ function expr_simple(ast, ls)
     return e
 end
 
+local function expr_keyword(ast, ls)
+    local kw, val
+    if ls.token == "TK_name" and ls:lookahead() == '=' then
+        local name = lex_str(ls)
+        kw = ast:literal(name)
+        lex_check(ls, '=')
+    end
+    val = expr(ast, ls)
+    return val, kw
+end
+
+local function expr_list_fix_last(ast, exps)
+    local n = #exps
+    if n > 0 then
+        exps[n] = ast:set_expr_last(exps[n])
+    end
+end
+
 function expr_list(ast, ls)
     local exps = { }
     exps[1] = expr(ast, ls)
     while lex_opt(ls, ',') do
         exps[#exps + 1] = expr(ast, ls)
     end
-    local n = #exps
-    if n > 0 then
-        exps[n] = ast:set_expr_last(exps[n])
-    end
+    expr_list_fix_last(ast, exps)
     return exps
+end
+
+function expr_list_with_keywords(ast, ls)
+    local kwargs, args = {}, {}
+    local val, kw = expr_keyword(ast, ls)
+    if kw then
+        kwargs[#kwargs+1] = { val, kw }
+    else
+        args[#args+1] = val
+    end
+    while lex_opt(ls, ',') do
+        local val, kw = expr_keyword(ast, ls)
+        if kw then
+            kwargs[#kwargs+1] = { val, kw }
+        else
+            args[#args+1] = val
+        end
+    end
+    expr_list_fix_last(ast, args)
+    return args, #kwargs > 0 and kwargs or nil
 end
 
 function expr_unop(ast, ls)
@@ -281,36 +316,19 @@ local function parse_repeat(ast, ls, line)
     return ast:repeat_stmt(cond, body, line, lastline)
 end
 
-local function expr_keyword(ast, ls, accept_keywords)
-    local kw, val
-    if ls.token == "TK_name" and accept_keywords and ls:lookahead() == '=' then
-        local name = lex_str(ls)
-        kw = ast:literal(name)
-        lex_check(ls, '=')
-    end
-    val = expr(ast, ls)
-    return val, kw
-end
-
 -- Parse function argument list.
 function parse_args(ast, ls, accept_keywords)
     local line = ls.linenumber
-    local args = { }
-    local kwargs
+    local args, kwargs
     if ls.token == '(' then
         if not LJ_52 and line ~= ls.lastline then
             err_syntax(ls, "ambiguous syntax (function call x new statement)")
         end
         ls:next()
-        while ls.token ~= ')' do
-            local val, kw = expr_keyword(ast, ls, accept_keywords)
-            if kw then
-                if not kwargs then kwargs = {} end
-                kwargs[#kwargs+1] = { val, kw }
-            else
-                args[#args+1] = val
-            end
-            if not lex_opt(ls, ',') then break end
+        if ls.token ~= ')' then -- Not f().
+            args, kwargs = expr_list_with_keywords(ast, ls)
+        else
+            args = { }
         end
         lex_match(ls, ')', '(', line)
     elseif ls.token == '{' then